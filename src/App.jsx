19c634c4968169ce429e7eb80fa18dc9240bd112
meherab111 <mehrabborno@gmail.com>
2025-04-09 22:10:12 +0600

    commited

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export const App = () => {
+
+  return (
+  <div>
+    <img src="squid-game.webp" alt="squid-game" width="50%" height="50%"/>
+    <h1>Name: Squid Game - 2</h1>
+    <h2>Imdb Rating: 8/10</h2>
+    <p>Description: A group of desperate people who are invited to participate in a deadly survival game for a large sum of money.</p>
+  </div>
+  )
+}
