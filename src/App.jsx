5a2b28a3fc5699627cda724abd2ebf954d782fd3
meherab111 <mehrabborno@gmail.com>
2025-04-10 13:30:35 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,37 @@ export const App = () => { //component
 }
 
 const MultipleCards = () => { // component
+  
+  const name = "Squid Game - 2" // using js 
+  const description = "A group of desperate people who are invited to participate in a deadly survival game for a large sum of money."
+  const genreFunc = () => {
+    return "Thriller, horrific"
+  }
+
+  // let age = 19
+
+  // let btnValue1 = "Watch Now"
+
+  // if(age < 18){ 
+  //   btnValue1 = "not available"
+  // }
+
+  const btnValue = (age) => {
+    return age >= 18 ? "watch now" : "not available"
+  }
+
   return (
     <div>
     <img src="squid-game.webp" alt="squid-game" width="50%" height="50%"/>
-    <h1>Name: Squid Game - 2</h1>
-    <h2>Imdb Rating: 8/10</h2>
-    <p>Description: A group of desperate people who are invited to participate in a deadly survival game for a large sum of money.</p>
+    <h1>Name: {name}</h1>
+    <h2>Imdb Rating: {4 + 4.5}</h2>
+    <p>Description: {description}</p>
+    <p>Genre: {genreFunc()}</p>
+    <button>{btnValue(17)}</button>
+    {/* <button>{btnValue1}</button> */}
+    {/* <button>{age >= 18 ? "watch now" : "not available"}</button> */}
   </div>
   )
+
+  // using js inside html, dynamic value insertion. arithmetic operation. function call. Conditional Operator.
 }
